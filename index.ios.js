6823e040ecc14d7451746f3699d5a9bbf04b071d
David DeWulf <dwdewul@gmail.com>
2017-07-18 16:06:40 -0500

    Initial commit. Performed system configuration and also built the initial components for the React Native app.

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+// Import a library to help create a Component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+// create a Component
+const App = () => (
+    <Header />
+);
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+/*
+* Creates a header that says "Albums"
+*/
+
+// import libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+// make a Component
+const Header = () => {
+    return <Text>Albums</Text>;
+};
+
+// make the component available to other parts of the App
+export default Header;
